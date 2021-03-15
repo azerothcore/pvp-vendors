859b9e79ef6668740d4edb27391fd8b5b7f5c161
Francesco Borzì <borzifrancesco@gmail.com>
2021-03-15 01:02:07 +0100

    feat: add AB and AV vendors

--- low-level/low-level-vendors.up.sql
+++ low-level/low-level-vendors.up.sql
@@ -1,7 +1,11 @@
-SET @GUID = 3200000; -- 2 vendors
+SET @GUID = 3200000; -- 6 vendors
 
 DELETE FROM `creature` WHERE `guid` IN (@GUID, @GUID + 1);
 INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`)
 VALUES
 (@GUID + 0, 14753, 0, 0, 0, 1, 1, 0, 1, -8764.57, 413.922, 103.919, 3.17492, 300, 0, 0, 52280, 0, 0, 0, 0, 0, '', 0),
-(@GUID + 1, 14754, 1, 0, 0, 1, 1, 0, 1, 1672.64, -4206.93, 56.3827, 3.437, 300, 0, 0, 52280, 0, 0, 0, 0, 0, '', 0);
+(@GUID + 1, 14754, 1, 0, 0, 1, 1, 0, 1, 1672.64, -4206.93, 56.3827, 3.437, 300, 0, 0, 52280, 0, 0, 0, 0, 0, '', 0),
+(@GUID + 2, 15127, 0, 0, 0, 1, 1, 0, 1, -8770.65, 422.256, 103.922, 3.7836, 300, 0, 0, 52280, 0, 0, 0, 0, 0, '', 0),
+(@GUID + 3, 13217, 0, 0, 0, 1, 1, 0, 1, -8777.75, 418.651, 103.921, 6.13339, 300, 0, 0, 57420, 0, 0, 0, 0, 0, '', 0),
+(@GUID + 4, 13219, 1, 0, 0, 1, 1, 0, 1, 1650.47, -4194.05, 56.383, 4.99195, 300, 0, 0, 57420, 0, 0, 0, 0, 0, '', 0),
+(@GUID + 5, 15126, 1, 0, 0, 1, 1, 0, 1, 1639.24, -4202.39, 56.3831, 5.68388, 300, 0, 0, 52280, 0, 0, 0, 0, 0, '', 0);
